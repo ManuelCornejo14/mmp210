4972da565ca8f52454fde4954af98420bff818d9
ManuelCornejo14 <60408951+ManuelCornejo14@users.noreply.github.com>
2020-02-11 07:59:41 -0800

    self portrait done

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,64 +1,52 @@
-// self portrait
+/* 
+    self portrait
+    by Manuel Cornejo 
+    2/11/2020
+*/
+
 function setup(){
-	createCanvas(900, 500);
+    createCanvas(900, 310);
 }
 
 function draw(){
-	background('lightblue');
+    background('lightblue');
+//neck
 
-    // shapes with xy coordinate + size
+fill('lightpink');
+rect(400, 200, 100, 100);
 
-fill('green');
-stroke('black');
+//hair
+fill('black')
+ellipse(450, 75, 125, 100);
+//face
+fill('orange');
+ellipse(450, 150, 150, 175);
+
+//eyes
+
+fill('white');
+//right eye
+ellipse(485, 130, 35, 35);
+//lefteye
+ellipse(415, 130, 35, 35);   
+
+//pupils
+fill('black');
+//right
+ellipse(485, 130, 20, 20);
+//left
+ellipse(415, 130, 20, 20);
+
+//mouth
+fill('red');
+strokeWeight(3);
+arc(450, 195, 50, 30, 190, 190);
+ 
 
 
-    // face area
-	ellipse(450, 180, 200); // face
-	fill('green');
-    ellipse(400, 160, 20); // right eye
-    ellipse(500, 160, 20); // left eye
-
-	rectMode(CENTER);
-	rect(200, 220, 100, 20, 10);
 
 
-    // right ear
-	arc(545, 180, 40, 50, -HALF_PI, HALF_PI);
-	
 
-    // shapes that are only xy points
 
-    // hair
-    line(150, 70, 180, 100);
-    line(160, 70, 180, 100);
-    line(170, 70, 180, 100);
-    line(180, 70, 180, 100);
-    line(190, 70, 180, 100);
-    line(200, 70, 180, 100);
-    line(210, 70, 180, 100);
-    line(220, 70, 180, 100);
-    line(230, 70, 180, 100);
-    line(240, 70, 180, 100);
-    line(250, 70, 180, 100);
-    line(260, 70, 180, 100);
-    line(270, 70, 180, 100);
-    line(280, 70, 180, 100);
-    line(290, 70, 180, 100);
-    line(300, 70, 180, 100);
-    line(310, 70, 180, 100);
-    line(320, 70, 180, 100);
-    line(330, 70, 180, 100);
-    line(340, 70, 180, 100);
-    line(350, 70, 180, 100);
-    line(360, 70, 180, 100);
-    line(370, 70, 180, 100);
-    line(380, 70, 180, 100);
-    line(390, 70, 180, 100);
-    line(400, 70, 180, 100);
-    line(410, 70, 180, 100);
-    line(420, 70, 180, 100);
-    line(430, 70, 180, 100);
-
-   
 
 }
